3ac35680b7b6d37d3cebb7bbe951d3eb5abee46b
Ben Nolan <ben@nolanconsul.com>
2015-02-25 18:23:27 +1300

    Production environment fixes.

--- server.js
+++ server.js
@@ -19,17 +19,20 @@ cors = require('cors');
 var Env = require('./lib/env');
 
 function Server(folder, port) {
-  var httpServer;
   this.folder = folder;
   this.port = port;
   this.restartServer = __bind(this.restartServer, this);
   this.onLoaded = __bind(this.onLoaded, this);
+
   console.log("[server] Serving scenes in '" + this.folder + "' on port " + this.port + "...");
+
   this.webServer = express();
   this.webServer.use(cors());
   this.webServer.use(express["static"](this.folder));
-  httpServer = http.createServer(this.webServer);
+
+  var httpServer = http.createServer(this.webServer);
   httpServer.listen(port);
+
   this.websocketServer = new WebsocketServer(httpServer);
   this.websocketServer.listen();
 
@@ -39,9 +42,11 @@ function Server(folder, port) {
 
   this.loadAllScenes();
 
-  require('dns').lookup(require('os').hostname(), function (err, add, fam) { 
-    console.log('\nOpen the following url to view your scenes:\n\thttp://client.scenevr.com/?connect=' + add + ':' + port + '/index.xml\n'); 
-  })
+  if(Env.isDevelopment()){
+    require('dns').lookup(require('os').hostname(), function (err, add, fam) { 
+      console.log('\nOpen the following url to view your scenes:\n\thttp://client.scenevr.com/?connect=' + add + ':' + port + '/index.xml\n'); 
+    })
+  }
 }
 
 Server.prototype.loadAllScenes = function() {
@@ -51,7 +56,9 @@ Server.prototype.loadAllScenes = function() {
 
     if(files.length == 0){
       console.log("[server] Error. No scene files found in " + _this.folder);
-      process.exit(-1);
+      if(Env.isDevelopment()){
+        process.exit(-1);
+      }
     }
 
     var indexXml = new IndexScene(files).toXml();
