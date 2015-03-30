1e6233d01bef4da51072dfd8d7044d92f4afbdb0
Ben Nolan <ben@nolanconsul.com>
2015-03-30 15:15:04 +1300

    Semistandard support. Added server#start.

--- server.js
+++ server.js
@@ -1,118 +1,117 @@
 #!/usr/bin/env node
 
-var IndexScene, Reflector, Scene, Server, WebsocketServer, cors, express, fs, glob, http, path, scenePath,
-  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-
 var _ = require('underscore');
-
-Reflector = require('./lib/reflector');
-WebsocketServer = require('./lib/websocket_server');
-Scene = require('./elements/scene');
-IndexScene = require('./lib/index_scene');
-path = require('path');
-fs = require('fs');
-glob = require('glob');
-express = require('express');
-http = require("http");
-cors = require('cors');
-
+var Reflector = require('./lib/reflector');
+var WebsocketServer = require('./lib/websocket_server');
+var Scene = require('./elements/scene');
+var IndexScene = require('./lib/index_scene');
+var path = require('path');
+var fs = require('fs');
+var glob = require('glob');
+var express = require('express');
+var http = require('http');
+var cors = require('cors');
 var Env = require('./lib/env');
 
-function Server(folder, port) {
+function Server (folder, port) {
   this.folder = folder;
   this.port = port;
-  this.restartServer = __bind(this.restartServer, this);
-  this.onLoaded = __bind(this.onLoaded, this);
+}
 
-  console.log("[server] Serving scenes in '" + this.folder + "' on port " + this.port + "...");
+Server.prototype.start = function () {
+  var self = this;
+
+  console.log('[server] Serving scenes in \'' + this.folder + '\' on port ' + this.port + '...');
 
   this.webServer = express();
   this.webServer.use(cors());
   this.webServer.use(express.static(this.folder));
 
   var httpServer = http.createServer(this.webServer);
-  httpServer.listen(port);
+  httpServer.listen(this.port);
 
   this.websocketServer = new WebsocketServer(httpServer);
   this.websocketServer.listen();
 
-  if(Env.supportsAutoReload()){
-    this.restart = _.throttle(this.restartServer, 1000, {trailing: false});
+  if (Env.supportsAutoReload()) {
+    this.restart = _.throttle(this.restartServer.bind(this), 1000, {trailing: false});
   }
 
   this.loadAllScenes();
 
-  if(Env.isDevelopment()){
-    require('dns').lookup(require('os').hostname(), function (err, add, fam) { 
-      console.log('\nOpen the following url to view your scenes:\n\thttp://client.scenevr.com/?connect=' + add + ':' + port + '/index.xml\n'); 
+  if (Env.isDevelopment()) {
+    require('dns').lookup(require('os').hostname(), function (err, addr, fam) {
+      var url = err ? 'localhost:' + self.port : addr + ':' + self.port;
+      console.log('\nOpen the following url to view your scenes:\n\thttp://client.scenevr.com/?connect=' + url + '/index.xml\n');
     });
   }
-}
+};
 
-Server.prototype.loadAllScenes = function() {
-  var _this = this;
+Server.prototype.loadAllScenes = function () {
+  var self = this;
 
-  glob(this.folder + "/*.xml", {}, function(er, files){
-
-    if(files.length === 0){
-      console.log("[server] Error. No scene files found in " + _this.folder);
-      if(Env.isDevelopment()){
+  glob(this.folder + '/*.xml', {}, function (err, files) {
+    if (err || (files.length === 0)) {
+      console.log('[server] Error. No scene files found in ' + self.folder);
+      if (Env.isDevelopment()) {
         process.exit(-1);
       }
     }
 
     var indexXml = new IndexScene(files).toXml();
 
-    Scene.load(indexXml, function(scene) {
-      _this.onLoaded(scene, '/index.xml');
+    Scene.load(indexXml, function (scene) {
+      self.onLoaded(scene, '/index.xml');
     });
 
-    files.forEach(function(filename) {
-      Scene.load(filename, function(scene){
-        _this.onLoaded(scene, '/' + path.basename(filename));
+    files.forEach(function (filename) {
+      Scene.load(filename, function (scene) {
+        self.onLoaded(scene, '/' + path.basename(filename));
 
-        if(Env.supportsAutoReload()){
-          fs.watch(filename, _this.restart);
+        if (Env.supportsAutoReload()) {
+          fs.watch(filename, self.restart);
         }
       });
     });
   });
 };
 
-Server.prototype.onLoaded = function(scene, filename) {
-  console.log("[server]  * Loaded '" + filename + "'");
+Server.prototype.onLoaded = function (scene, filename) {
+  console.log('[server]  * Loaded \'' + filename + '\'');
 
   var reflector = new Reflector(scene, filename);
   this.websocketServer.reflectors[filename] = reflector;
   reflector.start();
 };
 
-if(Env.supportsAutoReload()){
-  Server.prototype.restartServer = function() {
-    var filename, reflector, _ref;
-    console.log("[server] Restarting server on file change.");
-    _ref = this.websocketServer.reflectors;
-    for (filename in _ref) {
-      reflector = _ref[filename];
-      reflector.emit('<event name="restart" />');
-      reflector.stop();
-      reflector.scene.stop();
-      delete reflector.scene;
-    }
-    return setTimeout((function(_this) {
-      return function() {
-        _this.websocketServer.clearReflectors();
-        return _this.loadAllScenes();
-      };
-    })(this), 250);
-  };
-}
+Server.prototype.restartServer = function () {
+  var self = this;
 
-scenePath = process.argv[2];
+  console.log('[server] Restarting server on file change.');
+
+  var _ref = this.websocketServer.reflectors;
+  var filename;
+
+  for (filename in _ref) {
+    var reflector = _ref[filename];
+    reflector.emit('<event name="restart" />');
+    reflector.stop();
+    reflector.scene.stop();
+    delete reflector.scene;
+  }
+
+  setTimeout(function () {
+    self.websocketServer.clearReflectors();
+    self.loadAllScenes();
+  }, 250);
+};
+
+var scenePath = process.argv[2];
 
 if (!scenePath) {
-  console.log("Usage: scenevr [scenedirectory]");
+  console.log('Usage: scenevr [scenedirectory]');
   process.exit(-1);
 }
 
-new Server(scenePath, Env.getPort());
+var server = new Server(scenePath, Env.getPort());
+server.start();
