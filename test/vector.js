46c6d2442efc8b90ddcb38109d4d47fdfb61eac9
Ben Nolan <ben@nolanconsul.com>
2015-03-30 22:33:11 +1300

    Fix tests and satisfy semistandard.

--- test/vector.js
+++ test/vector.js
@@ -1,32 +1,35 @@
 var test = require('tape');
 var Vector = require('../lib/vector.js');
 
-test('should set and get', function(t) {
+test('should set and get', function (t) {
   var v = new Vector(1, 2, 3);
-  expect(v.x).toEqual(1);
-  expect(v.y).toEqual(2);
-  return expect(v.z).toEqual(3);
+  t.equal(v.x, 1);
+  t.equal(v.y, 2);
+  t.equal(v.z, 3);
+  t.end();
 });
 
-test('should clone', function() {
+test('should clone', function (t) {
   var v;
   v = new Vector(1, 2, 3);
   v.clone().x = 100;
-  return expect(v.x).toEqual(1);
+  t.equal(v.x, 1);
+  t.end();
 });
 
-test('should trigger onChanged', function() {
-  var v;
-  v = new Vector(1, 2, 3);
-  v.onChanged = jasmine.createSpy("onChanged");
-  expect(v.onChanged).not.toHaveBeenCalled();
+test('should trigger onChanged', function (t) {
+  t.plan(1);
+
+  var v = new Vector(1, 2, 3);
+  v.onChanged = function () {
+    t.ok(true);
+  };
   v.x += 100;
-  return expect(v.onChanged).toHaveBeenCalled();
 });
 
-test('should parse', function() {
+test('should parse', function (t) {
   var v;
-  v = Vector.fromString("1 2 3");
-  return expect(v.z).toEqual(3);
+  v = Vector.fromString('1 2 3');
+  t.equal(v.z, 3);
+  t.end();
 });
-
