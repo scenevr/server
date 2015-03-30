80ae2ec6acc489fec436e59bc8ba9ba1df563e5e
Ben Nolan <ben@nolanconsul.com>
2015-03-30 15:06:11 +1300

    Semistandard compliance fixes.

--- publickey.js
+++ publickey.js
@@ -1,4 +1,3 @@
-
 // Public key for login.scenevr.com to validate JWT messages
 
-module.exports = "-----BEGIN PUBLIC KEY-----\nMFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAN3rv4dWaUMlmvlXkQFQjhM6euDB4Jrm\nKoh4wPGXvnBGEQVJerQHOV9uoqPtmVe2c08hAN+6XkpXdvtT8RKYucMCAwEAAQ==\n-----END PUBLIC KEY-----\n";
+module.exports = '-----BEGIN PUBLIC KEY-----\nMFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAN3rv4dWaUMlmvlXkQFQjhM6euDB4Jrm\nKoh4wPGXvnBGEQVJerQHOV9uoqPtmVe2c08hAN+6XkpXdvtT8RKYucMCAwEAAQ==\n-----END PUBLIC KEY-----\n';
