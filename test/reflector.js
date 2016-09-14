32dd9f9a617b811ddfc24cd7e3b98d71320c4a50
Ben Nolan <bnolan@gmail.com>
2016-09-14 17:56:09 +1200

    Refactoring reflector to use micro-dom and scene-streamer.

--- test/reflector.js
+++ test/reflector.js
@@ -1,8 +1,9 @@
-var test = require('tape');
-var Reflector = require('../lib/reflector');
-var ChatChannel = require('../lib/chat-channel');
-var Document = require('scene-dom').Document;
-var Scene = require('scene-dom').Scene;
+const test = require('tape');
+const Reflector = require('../lib/reflector');
+const ChatChannel = require('../lib/chat-channel');
+const microdom = require('micro-dom');
+
+const Document = microdom.Document;
 
 function createObserver (name) {
   return {
@@ -11,18 +12,26 @@ function createObserver (name) {
     setPlayer: function () {},
     player: d.createElement('player'),
     sendMessage: function () {},
+    send: function () {},
     destroy: function () {}
   };
 }
 
-var d = Document.createDocument();
+const GLOBALS = {
+  HTMLElement: microdom.HTMLElement,
+  MutationObserver: require('micro-dom').MutationObserver
+};
+
+var d = new Document();
+d.body.innerHTML = '<a-scene></a-scene>';
+
 var o = createObserver('ben');
-var r = new Reflector(d);
+var r = new Reflector(GLOBALS, d);
 
 test('ctor#create', function (t) {
   t.ok(r instanceof Reflector);
   t.ok(r.chatChannel instanceof ChatChannel);
-  t.ok(r.scene instanceof Scene);
+  t.ok(r.document instanceof Document);
   t.end();
 });
 
@@ -31,25 +40,21 @@ test('broadcast', function (t) {
   t.end();
 });
 
-test('emit', function (t) {
-  r.emit('<boop />');
-  t.end();
-});
-
 var o2 = createObserver('joe');
 
 test('addObserver', function (t) {
   r.addObserver(o);
-  t.equal(1, r.observerCount());
+  t.equal(1, r.observerCount);
   r.addObserver(o2);
-  t.equal(2, r.observerCount());
+  t.equal(2, r.observerCount);
   t.end();
 });
 
 test('removeObserver', function (t) {
   r.removeObserver(o2);
-  t.equal(1, r.observerCount());
+  t.equal(1, r.observerCount);
   t.ok(r.hasObserver(o));
   t.notOk(r.hasObserver(o2));
+  t.notOk(r.empty);
   t.end();
 });
